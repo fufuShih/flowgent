388f49f96be7319670ff36d812efbb73e8e9f373
FelixShih <zx8714245@gmail.com>
2024-12-14 06:26:24 +0800

    fix: update App component layout and adjust body display style

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -2,9 +2,9 @@ import { FlowEditor } from "@/components/FlowEditor"
 
 function App() {
   return (
-    <>
+    <div className="w-full min-h-screen">
       <FlowEditor />
-    </>
+    </div>
   )
 }
 
